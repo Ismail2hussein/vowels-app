3bd0a9d1351150c6d126be50a5ecc45d72e68307
Ismail Hussein <133203884+Ismail2hussein@users.noreply.github.com>
2023-06-21 11:58:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const inputText = document.getElementById("input-text");
+const countButton = document.getElementById("count-btn");
+const vowelCount = document.getElementById("vowel-count");
 
+function countVowels() {
+  const text = inputText.value.toLowerCase();
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    if (vowels.includes(text[i])) {
+      count++;
+    }
+  }
+
+  vowelCount.textContent = count;
+}
+countButton.addEventListener("click", countVowels);
